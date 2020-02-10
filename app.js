fcb16f12c8f9b1600e08f5df29bce528e36e30ca
Patrick Nguyen <patrick.nguyen154@gmail.com>
2020-02-10 14:09:09 -0700

    added year of birth

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // eslint-disable-next-line strict
 function createGreeting(name, age) {
-  return `Hi, my name is ${name}, and I\'m ${age} years old.`;
+  const yearOfBirth = 2020 - age;
+  return `Hi, my name is ${name}, and I\'m ${age} years old. I was born in ${yearOfBirth}`;
 }
 
 const greeting1 = createGreeting('Patrick', 24);
