8665faca5f8cc55273c3292bb7aca3503724b66e
Sacha Sedriks <sachased@gmail.com>
2020-02-10 14:27:32 -0800

    adding and modfiying shizz: app-backup.js. added .vscode json for debugging. newtry.js

--- app-backup.js
+++ app-backup.js
@@ -1,17 +1,20 @@
 // eslint-disable-next-line strict
 function createGreeting(name, age) {
-  if (age < 0) {
+  const yob = getYearOfBirth(age);
+  return `Hi, my name is ${name}, and I'm ${age} years old. I was born in ${yob}`;
+  /* if (age < 0) {
     throw new Error ('Age can not be negative.');
   } else {
     const yob = getYearOfBirth(age);
     return `Hi, my name is ${name}, and I'm ${age} years old. I was born in ${yob}`;
   }
+  */
 }
 
 function getYearOfBirth(age) {
   return 2020 - age;
 }
-
+console.log('Hi');
 createGreeting('Patrick', 24);
 
 /*try {
