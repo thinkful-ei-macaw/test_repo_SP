63ed38f0133edc9961f09087e636b562daf162cf
Patrick Nguyen <patrick.nguyen154@gmail.com>
2020-02-10 14:00:51 -0700

    mod js

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log('Hi, my name is Patrick, and I\'m 24 years old.')
+// eslint-disable-next-line strict
+function createGreeting() {
+  console.log('Hi, my name is Patrick, and I\'m 24 years old.');
+}
+
+createGreeting();
