06a3c6c07b937ff0417df63ba506fee63ea5bee7
Patrick Nguyen <patrick.nguyen154@gmail.com>
2020-02-10 14:37:16 -0700

    check it out

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 // eslint-disable-next-line strict
 function createGreeting(name, age) {
-  const yob = getYearOfBirth(age);
-  return `Hi, my name is ${name}, and I\'m ${age} years old. I was born in ${yob}`;
+  if (age < 0) {
+    throw new Error ('Age can not be negative.');
+  } else {
+    const yob = getYearOfBirth(age);
+    return `Hi, my name is ${name}, and I'm ${age} years old. I was born in ${yob}`;
+  }
 }
 
 function getYearOfBirth(age) {
-    return 2020 - age;
+  return 2020 - age;
 }
 
-const greeting1 = createGreeting('Patrick', 24);
-console.log(greeting1);
+createGreeting('Patrick', 24);
+
+/*try {
+  const greeting1 = createGreeting('Patrick', 24);
+} catch (e) { 
+  console.log('your mama');
+  console.log(`${e.name}: ${e.message}`);
+} finally {
+  console.log(greeting1);
+
+console.log(greeting1); */
+
+
 
