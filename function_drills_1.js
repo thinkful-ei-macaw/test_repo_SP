48bf0e6316fa01c70dd91f64de2000c735ba31ba
Sacha Sedriks <sachased@gmail.com>
2020-02-10 20:50:50 -0800

    Func Drill 1, v7

--- function_drills_1.js
+++ function_drills_1.js
@@ -6,9 +6,20 @@ function createGreeting(name, age) {
 }
 
 function getYearOfBirth (age) {
+  if (age < 0) {
+    throw new Error('Age can not be negative');
+  } // else
   return 2019 - age;
 }
 
+let greeting1; // *NB* when declared variable as `const`, got SyntaxError: Missing initializer in const declaration. *WHY?*
 
-const greeting1 = createGreeting('Sacha', 46);
+try {
+  // initially declared greeting1 variable in here only
+  // const greeting1 =
+  // but didn't work, because need to establish/declare variable outside the scope of try {}
+  greeting1 = createGreeting('Sacha', 46);
+} catch (err) {
+  console.log(`${err.name}: ${err.message}`);
+}
 console.log(greeting1);
