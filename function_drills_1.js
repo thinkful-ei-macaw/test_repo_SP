eed5e77017de2f52b238e52bff0ab80f206eb233
Sacha Sedriks <sachased@gmail.com>
2020-02-10 20:33:49 -0800

    Func Drill 1, v4

--- function_drills_1.js
+++ function_drills_1.js
@@ -1,8 +1,8 @@
 'use strict';
 
-function createGreeting() {
-  return `Hi, my name is Sacha and I'm — gulp — 46 years old.`;
+function createGreeting(name, age) {
+  return `Hi, my name is ${name} and I'm — gulp — ${age} years old.`;
 }
 
-const greeting1 = createGreeting();
+const greeting1 = createGreeting('Sacha', 46);
 console.log(greeting1);
