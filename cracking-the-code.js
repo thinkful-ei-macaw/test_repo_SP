292bf8758d524c32f3e5e52d250b0de394a1d4e2
Sacha Sedriks <sachased@gmail.com>
2020-02-11 19:38:24 -0800

    done cracking-the-code.js

--- cracking-the-code.js
+++ cracking-the-code.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// skipping for now ;P
+// from https://courses.thinkful.com/ei-js-v1/checkpoint/8
+
+function decode(word) {
+  // decode each word ... no need to put a `for` loop in here, because this function will be called within a .forEach() method, which has a built-in loop to parse every item in the array
+  let firstLetter = word.charAt(0);
+  if (firstLetter === 'a') {
+    return word.charAt(1);
+  } else if (firstLetter === 'b') {
+    return word.charAt(2);
+  } else if (firstLetter === 'c') {
+    return word.charAt(3);
+  } else if (firstLetter === 'd') {
+    return word.charAt(4);
+  } else {
+    return ' ';
+  }
+
+
+}
+
+// return decoded message
+
+const message = 'craft block argon meter bells brown croon droop';
+const words = message.split(' ');
+// console.log(words);
+
+words.forEach( word => console.log(decode(word)));
